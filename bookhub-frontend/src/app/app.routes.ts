deef3625d7e745e6a064ebebf15f3c3b2c9b448b
Julien LE BLAY <julien.leblay2026@campus-eni.fr>
2026-07-23 14:02:00 +0200

    A1003

--- bookhub-frontend/src/app/app.routes.ts
+++ bookhub-frontend/src/app/app.routes.ts
@@ -1,18 +1,23 @@
 import { Routes } from '@angular/router';
 import { Login } from './features/login/login';
-import { MainLayout } from './shared/components/main-layout/main-layout';
 import { Home } from './features/home/home';
+import { MainLayout } from './shared/components/main-layout/main-layout';
 
 export const routes: Routes = [
-  // Route par défaut (redirige la racine vers /login ou /home)
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-
-  // La route pour la page de connexion
+  // 1. Page de Login (seule, sans Header/Footer)
   { path: 'login', component: Login },
 
-  //  La route vers laquelle tu rediriges après la connexion
-  { path: 'home', component: Home },
+  // 2. Pages avec le Layout (avec Header/Footer)
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: 'home', component: Home },
+      //{ path: 'profile', component: Profile },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
 
-  // Route "catch-all" en cas d'URL inconnue (optionnel)
+  // Redirection par défaut
   { path: '**', redirectTo: 'login' },
 ];
--- bookhub-frontend/src/app/app.ts
+++ bookhub-frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './features/login/login';
+import { Footer } from './core/components/footer/footer';
+import { Header } from './core/components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
