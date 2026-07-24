149021f09cc1bc691894e3ddec2a7d52a6ef1325
Julien LE BLAY <julien.leblay2026@campus-eni.fr>
2026-07-24 15:54:59 +0200

    integration auth backend

--- bookhub-frontend/src/app/features/login/login.ts
+++ bookhub-frontend/src/app/features/login/login.ts
@@ -1,12 +1,12 @@
-import { ChangeDetectorRef, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { AuthService } from '../../core/services/auth-service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -27,14 +27,33 @@ export class Login {
       return;
     }
 
-    // Appels des deux arguments vers le service
-    this.authService.login(this.email, this.password);
+    // Réinitialisation du message d'erreur
+    this.errorMessage = '';
 
-    // PROVISOIRE !!! Sauvegarder l'email dans le navigateur pour le Header
-    localStorage.setItem('userEmail', this.email);
+    //Abonnement à l'observable renvoyé par le service
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        // Le serveur a validé les identifiants
+        // On enregistre le token et/ou les infos reçues du bac
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+        }
+        // Exemple d'enregistrement des infos utilisateur
+        localStorage.setItem('userEmail', this.email);
 
-    // Redirection après enregistrement
-    this.router.navigate(['/home']);
+        // Redirection vers /home une fois connecté
+        this.router.navigate(['/home']);
+      },
+      error: (error) => {
+        // Échec ! Mauvais mot de passe, utilisateur inconnu, etc.
+        console.error('Erreur de connexion :', error);
 
+        if (error.status === 401) {
+          this.errorMessage = 'Email ou mot de passe incorrect.';
+        } else {
+          this.errorMessage = 'Une erreur est survenue lors de la connexion.';
+        }
+      },
+    });
   }
 }
